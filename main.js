b41bdc4003728572bcd4cb313bad05e226058822
hellojeongso <84715469+hellojeongso@users.noreply.github.com>
2021-11-21 08:14:39 +0900

    Main-card

--- main.js
+++ main.js
@@ -1,9 +1,40 @@
-// Swiper
+// Swiper: Intro
 var swiper = new Swiper(".mySwiper", {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  observer: true,
+  observeParents: true,
+});
+
+
+// Slick: main-card
+$('.slick-card').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  arrows : false, 
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
 });
 
 // Aos
